61acafa7e7d062767ba0420aac52876fc3c2a226
Jack Mitchell <jack.danielle.mitchell@gmail.com>
2020-01-29 17:50:30 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // i already completed this on the prep course lesson, but I want to try it again now that I understand my mistakes better. Here's to hoping I don't make the same mistakes!! :don
 
 function theBeatlesPlay(musicians, instruments) {
- var array = []
+ var array = [];
  
  for(i = 0; i < musicians.length; i++) {
    var str = musicians[i] + " plays " + instruments[i];
