e68a7cd5fa5ab28328fcbe05844b886135d9b54c
Jack Mitchell <jack.danielle.mitchell@gmail.com>
2020-01-29 17:37:55 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-// add solution here
+// i already completed this on the prep course lesson, but I want to try it again now that I understand my mistakes better. Here's to hoping I don't make the same mistakes!! :don
+
+function theBeatlesPlay(array) {
+ var array = []
+ 
+}
